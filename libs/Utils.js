bcda9d983df53d637010cb4efb6849c851ce1035
Brandon Harris <bharris@gaijin.com>
2019-05-03 12:12:53 -0700

    * Add sha256

--- libs/Utils.js
+++ libs/Utils.js
@@ -103,6 +103,25 @@ class Utils {
         return config;
     }
 
+    /**
+     * Encrypt a text string
+     * @param message the string to encrypt
+     * @return {Promise<string>}
+     */
+    async static sha256(message) {
+        // encode as UTF-8
+        const msgBuffer = new TextEncoder('utf-8').encode(message);
+
+        // hash the message
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+        // convert ArrayBuffer to Array
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+        // convert bytes to hex string
+        return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+
+    }
 
     /**
      * Format a number as money.
