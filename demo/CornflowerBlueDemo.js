7c07059f370558441d6535de9c297ee3aff2c997
Brandon Harris <bharris@gaijin.com>
2019-06-21 11:18:52 -0700

    * Add SkipButton class

--- demo/CornflowerBlueDemo.js
+++ demo/CornflowerBlueDemo.js
@@ -218,23 +218,7 @@ class CornflowerBlueDemo {
     build() {
         const me = this;
 
-        this.skipbutton = new SimpleButton({
-            text: "Skip to Content",
-            classes: ['visually-hidden'],
-            id: 'content-jump',
-            hot: true,
-            focusin: function(e, self) {
-                self.button.removeClass('visually-hidden');
-            },
-            focusout: function(e, self) {
-                self.button.addClass('visually-hidden');
-            },
-            action: function(e, self) {
-                let url = location.href;
-                location.href = "#content-start";
-                history.replaceState(null,null,url);
-            }
-        });
+        this.skipbutton = new SkipButton(); // defaults are fine
 
         this.body.prepend(this.skipbutton.button);
 
