8fd49f7967b9d7c463a5b78d221bb5406edb65bf
Brandon Harris <bharris@gaijin.com>
2020-03-13 12:03:16 -0700

    * Last half of the MessageBox refactor

--- src/libs/components/info/ResultsContainer.js
+++ src/libs/components/info/ResultsContainer.js
@@ -1,7 +1,10 @@
-class ResultsContainer extends MessageBox {
+class ResultsContainer {
 
     static get DEFAULT_CONFIG() {
         return {
+            id : null, // Component id
+            classes: [], // Extra css classes to apply,
+
             errors: null, // array of errors
             warnings: null, // array of warning strings
             results: null, // array of result or success message strings
@@ -13,8 +16,7 @@ class ResultsContainer extends MessageBox {
      * @param config a dictionary object
      */
     constructor(config) {
-        config = Object.assign({}, ResultsBox.DEFAULT_CONFIG, config);
-        super(config);
+        this.config = Object.assign({}, ResultsContainer.DEFAULT_CONFIG, config);
     }
 
     /* CONSTRUCTION METHODS_____________________________________________________________ */
@@ -23,104 +25,51 @@ class ResultsContainer extends MessageBox {
      * Build the full messagebox container
      */
     buildContainer() {
-        super.buildContainer();
-
+        this.container = document.createElement('div');
+        
         this.container.classList.add('resultsbox');
+        for (let c of this.classes) {
+            this.container.classList.add(c);
+        }
 
         if ((this.errors) && (this.errors.length > 0)) {
-            this.container.appendChild(this.buildBox("errors"));
+            this.container.appendChild(new ErrorBox({
+                errors: this.errors,
+            }).container);
         }
 
         if ((this.results) && (this.results.length > 0)) {
-            this.container.appendChild(this.buildBox("results"));
+            this.container.appendChild(new SuccessBox({
+                results: this.results,
+            }).container);
         }
 
         if ((this.warnings) && (this.warnings.length > 0)) {
-            this.container.appendChild(this.buildBox("warnings"));
+            this.container.appendChild(new WarningBox({
+                warnings: this.warnings,
+            }).container);
         }
-
-    }
-
-    /**
-     * Build the specific box.
-     * @param type the type of box to create (errors|warnings|results)
-     * @return a DOM element
-     */
-    buildBox(type = 'results') {
-        let list,
-            icon,
-            title;
-
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.classList.add(type);
-
-        switch(type) {
-            case 'errors':
-                list = this.errors;
-                icon = this.erroricon;
-                title = this.errorstitle;
-                break;
-            case 'warnings':
-                list = this.warnings;
-                icon = this.warningicon;
-                title = this.warningstitle;
-                break;
-            case 'results':
-            default:
-                list = this.results;
-                icon = this.successicon;
-                title = this.successtitle;
-                break;
-        }
-
-        if (title) {
-            let telem = document.createElement('h4');
-            telem.innerHTML = title;
-            box.appendChild(telem);
-        }
-
-        let lbox = document.createElement('div');
-        lbox.classList.add('lbox');
-
-
-        if (icon) { lbox.appendChild(IconFactory.icon(icon)); }
-
-
-        let mlist = document.createElement('ul');
-        for (let text of list) {
-            let li = document.createElement('li');
-            li.innerHTML = text;
-            mlist.appendChild(li);
-        }
-
-        lbox.appendChild(mlist);
-
-        lbox.classList.add(`size-${list.length}`);
-
-        box.appendChild(lbox);
-        return box;
     }
 
     /* ACCESSOR METHODS_________________________________________________________________ */
 
+    get classes() { return this.config.classes; }
+    set classes(classes) { this.config.classes = classes; }
+
+    get container() {
+        if (!this._container) { this.buildContainer(); }
+        return this._container;
+    }
+    set container(container) { this._container = container; }
+
     get errors() { return this.config.errors; }
     set errors(errors) { this.config.errors = errors; }
 
-    get errorstitle() { return this.config.errorstitle; }
-    set errorstitle(errorstitle) { this.config.errorstitle = errorstitle; }
-
-    get erroricon() { return this.config.erroricon; }
-    set erroricon(erroricon) { this.config.erroricon = erroricon; }
+    get id() { return this.config.id; }
+    set id(id) { this.config.id = id; }
 
     get results() { return this.config.results; }
     set results(results) { this.config.results = results; }
-
-    get successtitle() { return this.config.successtitle; }
-    set successtitle(successtitle) { this.config.successtitle = successtitle; }
-
-    get successicon() { return this.config.successicon; }
-    set successicon(successicon) { this.config.successicon = successicon; }
 
     get warnings() { return this.config.warnings; }
     set warnings(warnings) { this.config.warnings = warnings; }
