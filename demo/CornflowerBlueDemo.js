4b180460018ef5eec90a41c98bade78ac5365412
Brandon Harris <bharris@gaijin.com>
2021-07-27 15:22:52 -1000

    * That's enough eyebleed for the day

--- demo/CornflowerBlueDemo.js
+++ demo/CornflowerBlueDemo.js
@@ -1539,8 +1539,123 @@ class CornflowerBlueDemo {
 
     grindCharts() {
 
-        let line = document.getElementById('chart-line'),
-            bar = document.getElementById('chart-bar');
+        let barcharts = document.getElementById('chart-bar'),
+            linecharts = document.getElementById('chart-line');
+
+
+        barcharts.classList.add('example');
+        barcharts.appendChild(new Chart({
+            label: "Letter Prevalence",
+            name: "letters",
+            data: [
+                {
+                    "letter": "A",
+                    "frequency": ".08167"
+                },
+                {
+                    "letter": "B",
+                    "frequency": ".01492"
+                },
+                {
+                    "letter": "C",
+                    "frequency": ".02782"
+                },
+                {
+                    "letter": "D",
+                    "frequency": ".04253"
+                },
+                {
+                    "letter": "E",
+                    "frequency": ".12702"
+                },
+                {
+                    "letter": "F",
+                    "frequency": ".02288"
+                },
+                {
+                    "letter": "G",
+                    "frequency": ".02015"
+                },
+                {
+                    "letter": "H",
+                    "frequency": ".06094"
+                },
+                {
+                    "letter": "I",
+                    "frequency": ".06966"
+                },
+                {
+                    "letter": "J",
+                    "frequency": ".00153"
+                },
+                {
+                    "letter": "K",
+                    "frequency": ".00772"
+                },
+                {
+                    "letter": "L",
+                    "frequency": ".04025"
+                },
+                {
+                    "letter": "M",
+                    "frequency": ".02406"
+                },
+                {
+                    "letter": "N",
+                    "frequency": ".06749"
+                },
+                {
+                    "letter": "O",
+                    "frequency": ".07507"
+                },
+                {
+                    "letter": "P",
+                    "frequency": ".01929"
+                },
+                {
+                    "letter": "Q",
+                    "frequency": ".00095"
+                },
+                {
+                    "letter": "R",
+                    "frequency": ".05987"
+                },
+                {
+                    "letter": "S",
+                    "frequency": ".06327"
+                },
+                {
+                    "letter": "T",
+                    "frequency": ".09056"
+                },
+                {
+                    "letter": "U",
+                    "frequency": ".02758"
+                },
+                {
+                    "letter": "V",
+                    "frequency": ".00978"
+                },
+                {
+                    "letter": "W",
+                    "frequency": ".02360"
+                },
+                {
+                    "letter": "X",
+                    "frequency": ".00150"
+                },
+                {
+                    "letter": "Y",
+                    "frequency": ".01974"
+                },
+                {
+                    "letter": "Z",
+                    "frequency": ".00074"
+                }
+            ]
+        }).container);
+
+
 
     }
 
--- src/libs/chart/Chart.js
+++ src/libs/chart/Chart.js
@@ -5,6 +5,12 @@ class Chart {
             id: null,
             classes: [],
             label: null,
+            source: null,
+            data: [],
+            svgId: null,
+            sourcetype: "application/json",
+            sourcemethod: 'GET',
+            charttype: 'bar',
             help: null
         };
     }
@@ -20,9 +26,85 @@ class Chart {
     constructor(config) {
         this.config = Object.assign({}, Chart.DEFAULT_CONFIG, config);
         if (!this.id) { this.id = `chart-${CFBUtils.getUniqueKey(5)}`; }
+        if (!this.svgId) { this.svgId = `chart-${this.charttype}-${CFBUtils.getUniqueKey(5)}`; }
+    }
+
+
+    get sourcebody() {
+        if (this.sourcemethod.toLowerCase() === 'get') { return null; }
+        return JSON.stringify({
+            id: this.id
+        })
     }
 
     /* CORE METHODS_____________________________________________________________________ */
+
+    compile() {
+        // This is mostly to create data portability
+        console.log(`svg#${this.svgId}`);
+        this.d3svg = d3.select(`svg#${this.svgId}`);
+
+        this.d3margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+        this.d3width = + this.d3svg.attr("width") - this.d3margin.left - this.d3margin.right;
+        this.d3height = + this.d3svg.attr("height") - this.d3margin.top - this.d3margin.bottom;
+
+        this.d3x = d3.scaleBand().rangeRound([0, this.d3width]).padding(0.1);
+        this.d3y = d3.scaleLinear().rangeRound([this.d3height, 0]);
+        this.d3g = this.d3svg.append("g");
+
+        this.d3g.attr("transform", `translate(${this.d3margin.left}, ${this.d3margin.top})`);
+
+        this.chart();
+
+    }
+
+    chart() {
+        this.d3x.domain(this.data.map((d) => { return d.letter; }));
+        this.d3y.domain([0, d3.max(this.data, (d) => { return d.frequency; })]);
+
+        this.d3g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", `translate(0, ${this.d3height})`)
+            .call(d3.axisBottom(this.d3x));
+
+        this.d3g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(this.d3y).ticks(10, "%"))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Frequency");
+
+        this.d3g.selectAll(".bar")
+            .data(this.data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", (d) => { return this.d3x(d.letter); })
+            .attr("y", (d) => { return this.d3y(d.frequency); })
+            .attr("width", this.d3x.bandwidth())
+            .attr("height", (d) => { return this.d3height - this.d3y(d.frequency); });
+    }
+
+    load(callback) {
+        fetch(this.source, {
+            method: this.sourcemethod,
+            headers: { 'Content-Type': this.sourcetype },
+            body: this.sourcebody
+        })
+            .then(response => response.json())
+            .then(data => {
+                if ((callback) && (typeof callback === 'function')) {
+                    callback(data);
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            });
+    }
+
 
 
     /* CONSTRUCTION METHODS_____________________________________________________________ */
@@ -33,16 +115,38 @@ class Chart {
     buildContainer() {
         this.container = document.createElement('div');
         this.container.classList.add('chart-container');
+        this.container.appendChild(this.svg);
+        window.setTimeout(() => {
+            if ((this.data) && (this.data.length > 0)) {
+                this.compile();
+            } else if (this.source) {
+                this.load((results) => {
+                    console.log('done loading');
+                    if (results.data) {
+                        this.data = results.data;
+                    }
+                    this.compile();
+                });
+            }
+        }, 200);
 
+    }
+
+    buildSVG() {
+        //this.svg = document.createElement('svg');
+        this.svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+
+        //this.svg.classList.add('chart', this.charttype);
+        this.svg.setAttribute('id', this.svgId);
+        this.svg.setAttribute('width', 960);
+        this.svg.setAttribute('height', 500);
     }
 
     /**
      * Builds the label
      */
     buildLabel() {
-
         if (!this.label) { return null; }
-
         this.labelobj = document.createElement('label');
         this.labelobj.setAttribute('for', this.id);
         this.labelobj.innerHTML = this.label;
@@ -69,6 +173,9 @@ class Chart {
 
     /* ACCESSOR METHODS_________________________________________________________________ */
 
+    get charttype() { return this.config.charttype; }
+    set charttype(charttype) { this.config.charttype = charttype; }
+
     get classes() { return this.config.classes; }
     set classes(classes) { this.config.classes = classes; }
 
@@ -77,6 +184,30 @@ class Chart {
         return this._container;
     }
     set container(container) { this._container = container; }
+
+    get d3g() { return this._d3g; }
+    set d3g(d3g) { this._d3g = d3g; }
+
+    get d3height() { return this._d3height; }
+    set d3height(d3height) { this._d3height = d3height; }
+
+    get d3margin() { return this._d3margin; }
+    set d3margin(d3margin) { this._d3margin = d3margin; }
+
+    get d3svg() { return this._d3svg; }
+    set d3svg(d3svg) { this._d3svg = d3svg; }
+
+    get d3width() { return this._d3width; }
+    set d3width(d3width) { this._d3width = d3width; }
+
+    get d3x() { return this._d3x; }
+    set d3x(d3x) { this._d3x = d3x; }
+
+    get d3y() { return this._d3y; }
+    set d3y(d3y) { this._d3y = d3y; }
+
+    get data() { return this.config.data; }
+    set data(data) { this.config.data; }
 
     get help() { return this.config.help; }
     set help(help) { this.config.help = help; }
@@ -95,6 +226,24 @@ class Chart {
         return this._labelobj;
     }
     set labelobj(labelobj) { this._labelobj = labelobj; }
+
+    get source() { return this.config.source; }
+    set source(source) { this.config.source = source; }
+
+    get sourcemethod() { return this.config.sourcemethod; }
+    set sourcemethod(sourcemethod) { this.config.sourcemethod = sourcemethod; }
+
+    get sourcetype() { return this.config.sourcetype; }
+    set sourcetype(sourcetype) { this.config.sourcetype = sourcetype; }
+
+    get svg() {
+        if (!this._svg) { this.buildSVG(); }
+        return this._svg;
+    }
+    set svg(svg) { this._svg = svg; }
+
+    get svgId() { return this.config.svgId; }
+    set svgId(svgId) { this.config.svgId = svgId; }
 
 }
 window.Chart = Chart;
