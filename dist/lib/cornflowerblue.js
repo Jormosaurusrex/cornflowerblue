b1e8e446ac5ba81cd320d7b345387781910c569a
Brandon Harris <bharris@gaijin.com>
2022-01-02 11:35:37 -1000

    Checkpoint

--- dist/lib/cornflowerblue.js
+++ dist/lib/cornflowerblue.js
@@ -1,4 +1,4 @@
-/*! Cornflower Blue - v0.1.1 - 2021-12-18
+/*! Cornflower Blue - v0.1.1 - 2021-12-19
 * http://www.gaijin.com/cornflowerblue/
 * Copyright (c) 2021 Brandon Harris; Licensed MIT */
 class CFBUtils {
